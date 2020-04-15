09b7d614fe3f0878617d68096aeb3e6c99364af0
Fidel_Yiu <1302139012@qq.com>
2020-04-15 10:50:35 +0800

    样式修改

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        三国趣闻
+      </a>
+      <ul id="nav" class="nav justify-content-center">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">首页</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">关于</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">友链</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">描述</a>
+        </li>
+      </ul>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
   #nav a {
-    font-weight: bold;
-    color: #2c3e50;
+    color: #504b4d;
   }
 
   #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <empty-div></empty-div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import EmptyDiv from "@/components/EmptyDiv";
+    import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {
+            HelloWorld,
+            EmptyDiv
+        }
+    }
 </script>
